11eff1a53149ebb3773b4e0f2cb3444b84edea8e
SherbakovSAF <sherbakovsaf@gmail.com>
2023-10-10 00:10:20 +0300

    Создание инпут компонента

---
+++ src/components/InputCurrency.vue
@@ -0,0 +1,15 @@
+<template>
+     <div>
+          <input autofocus type="number" placeholder="Введите значение для конвертации" class="input"
+               :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+     </div>
+</template>
+<script lang="ts">
+export default {
+     name: 'InputCurrency',
+     props: {
+          modelValue: {type: String, required: true}
+     }, 
+     emits: ['update:modelValue'],
+}
+</script>
